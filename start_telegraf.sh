0e909e79fafaf26cc1e100cee5fe1fa19ad4275b
Shihta Kuan <shihta.kuan@gmail.com>
2020-03-10 15:43:00 +0800

    Add qswitch.conf

--- start_telegraf.sh
+++ start_telegraf.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./src/github.com/influxdata/telegraf/telegraf --config ./conf/cpu_mem.conf
+# ./src/github.com/influxdata/telegraf/telegraf --config ./conf/cpu_mem.conf
+cd src/github.com/influxdata/telegraf
+make
+cd -
+./src/github.com/influxdata/telegraf/telegraf --config ./conf/qswitch.conf
