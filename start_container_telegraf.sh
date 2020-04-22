cf7949eedf5e87bf97104faf332d6123c5efe1e9
Shihta Kuan <shihta.kuan@gmail.com>
2020-04-22 15:13:08 +0800

    Add configs for cell1 env

--- start_container_telegraf.sh
+++ start_container_telegraf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -tid -p 9273:9273 -v $PWD/conf/qswitch.conf:/etc/telegraf/telegraf.conf 10.102.50.206:5000/nfvd/telegraf:$1
+docker run -tid -p 9273:9273 -v $PWD/conf/qswitch.conf:/etc/telegraf/telegraf.conf --log-driver json-file --log-opt max-size=10m --log-opt max-file=3 10.102.50.206:5000/nfvd/telegraf:$1
--- start_telegraf.sh
+++ start_telegraf.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # ./src/github.com/influxdata/telegraf/telegraf --config ./conf/cpu_mem.conf
+
 cd src/github.com/influxdata/telegraf
 make
 cd -
-./src/github.com/influxdata/telegraf/telegraf --config ./conf/qswitch.conf
+# ./src/github.com/influxdata/telegraf/telegraf --config ./conf/qswitch.conf
+
+./src/github.com/influxdata/telegraf/telegraf --config ./conf/cell1-switch.conf
