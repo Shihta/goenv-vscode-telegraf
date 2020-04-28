0cb85854b6773d8bd578a8e8cae5d0e4456bbb27
Shihta Kuan <shihta.kuan@gmail.com>
2020-04-28 22:41:22 +0800

    Organize dev-env configurations

--- start_container_telegraf_server.sh
+++ start_container_telegraf_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -tid -p 9273:9273 -e SERIAL_NUMBER=QTFCR27330032 -e SERVER_ID=5 --privileged -v /dev:/dev:ro -v /proc:/hostfs/proc:ro -v /run/udev:/run/udev:ro -e HOST_PROC=/hostfs/proc -v $PWD/conf/dev_server_template.conf:/etc/telegraf/telegraf.conf --log-driver json-file --log-opt max-size=10m --log-opt max-file=3 10.102.50.206:5000/nfvd/telegraf:$1
+# local
+# docker run -tid -p 9273:9273 -e SERVER_ID=5 --privileged -v /dev:/dev:ro -v /proc:/hostfs/proc:ro -v /run/udev:/run/udev:ro -e HOST_PROC=/hostfs/proc -v $PWD/conf/dev-server-local.conf:/etc/telegraf/telegraf.conf --log-driver json-file --log-opt max-size=10m --log-opt max-file=3 10.102.50.206:5000/nfvd/telegraf:$1
+
+# remote
+docker run -tid -p 9274:9273 -v $PWD/conf/dev-server-remote.conf:/etc/telegraf/telegraf.conf --log-driver json-file --log-opt max-size=10m --log-opt max-file=3 10.102.50.206:5000/nfvd/telegraf:$1
